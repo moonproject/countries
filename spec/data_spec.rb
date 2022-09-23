52da4e0cf554299e450eedc703506551a7a6ec0b
Damien Metzger <damien@mooncard.co>
2022-09-23 12:25:29 +0200

    add missing dependencies & fix specs

--- spec/data_spec.rb
+++ spec/data_spec.rb
@@ -41,7 +41,7 @@ describe ISO3166::Data do
     it 'returns an array' do
       data = ISO3166::Data.codes
       expect(data).to be_a Array
-      expect(data.size).to eq 249
+      expect(data.size).to eq 250
     end
   end
 
